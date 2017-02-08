61e93f63ef8d31324cc52e8a7bcf74cd5a56fd8c
Mehul Harry <mharry9999@gmail.com>
2017-02-08 15:00:29 -0800

    Added devextreme widgets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import notify from 'devextreme/ui/notify';
+import {Service, Company} from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Service]
 })
 export class AppComponent {
-  title = 'app works!';
+  name: String;
+  dataSource: Company[];
+
+  constructor(service: Service) {
+    this.dataSource = service.getCompanies();
+  }
+
+  sayHello(params){
+    if (params.validationGroup.validate().isValid) {
+      notify('Hello ' + this.name + ' !', "info", 2000 );
+    }
+  }
 }
